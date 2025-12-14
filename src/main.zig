34cb79e57ab1e2abbd38b786ca1b6425f64c4766
vedant-pandey <vedantpandey46@gmail.com>
2025-12-14 18:57:54 +0530

    Setup app state

--- src/main.zig
+++ src/main.zig
@@ -4,26 +4,50 @@ const std = @import("std");
 const ScreenWidth = 640;
 const ScreenHeight = 480;
 
+const AppState = struct {
+    window: sdl3.video.Window,
+    renderer: sdl3.render.Renderer,
+    screenWidth: usize,
+    screenHeight: usize,
+    initFlags: sdl3.InitFlags,
+
+    const Self = @This();
+
+    pub fn init(initFlags: sdl3.InitFlags, screenWidth: usize, screenHeight: usize) !*Self {
+        try sdl3.init(initFlags);
+        const window = try sdl3.video.Window.init("Procedural generation", screenWidth, screenHeight, .{
+            .always_on_top = true,
+            .mouse_focus = true,
+            .input_focus = true,
+            .resizable = true,
+        });
+        window.raise() catch unreachable;
+
+        const renderer = try sdl3.render.Renderer.init(window, null);
+
+        var state = AppState{
+            .window = window,
+            .renderer = renderer,
+            .screenWidth = screenWidth,
+            .screenHeight = screenHeight,
+            .initFlags = initFlags,
+        };
+
+        return &state;
+    }
+
+    pub fn deinit(self: *Self) void {
+        defer sdl3.shutdown();
+        defer sdl3.quit(self.initFlags);
+    }
+};
+
 pub fn main() !void {
-    defer sdl3.shutdown();
-
-    const init_flags = sdl3.InitFlags{ .video = true };
-    try sdl3.init(init_flags);
-    defer sdl3.quit(init_flags);
-
-    const window = try sdl3.video.Window.init("Procedural generation", ScreenWidth, ScreenHeight, .{
-        .always_on_top = true,
-        .mouse_focus = true,
-        .input_focus = true,
-        .resizable = true,
-    });
-    defer window.deinit();
-
-    window.raise() catch unreachable;
+    var state = try AppState.init(.{ .video = true }, ScreenWidth, ScreenHeight);
+    defer state.deinit();
 
     var quit = false;
     while (!quit) {
-
         while (sdl3.events.poll()) |event| {
             switch (event) {
                 .quit => quit = true,
